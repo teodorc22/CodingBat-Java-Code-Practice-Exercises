b5c400f80771f4d3f14bad45ab7bc80101c90484
Teodor Coste <47660750+teodorc22@users.noreply.github.com>
2023-03-13 16:02:05 +0200

    Create in1020.java

---
+++ Warmup-1/in1020.java
@@ -0,0 +1,8 @@
+//Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+
+public boolean in1020(int a, int b) {
+  if((a >= 10 && a <= 20) || (b >= 10 && b <= 20)) {
+    return true;
+  }
+  return false;
+  }
